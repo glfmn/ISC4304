9e26d59cf65d2c64994aa8dd1340bb2d3cc8a2bf
Gwen Lofman <Gwen@Lofman.io>
2018-04-28 22:21:07 -0400

    Show mandelbrot set with python script
    Capture C++ program's output which is redirected via stdout, and
    turn it into a numpy array in the proper format.

---
+++ lab/7/mandel.py
@@ -0,0 +1,40 @@
+"""Lab 7: Mandelbrot Set part 2
+
+USAGE: mandel.py <width> <height>
+
+Options:
+    -h, --help    Show this screen
+    --version     Show the version number
+
+Print the mandelbrot set to screen using a C++ utility to generate the data
+faster than a native python application can.
+"""
+
+from docopt import docopt
+import numpy as np
+from subprocess import Popen, PIPE
+from pylab import imshow, show
+
+import pixmap
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='mandel2 0.1.0')
+
+    width = args["<width>"]
+    height = args["<height>"]
+
+    process = Popen([
+        "./mandel/mandel",
+        width,
+        height,
+        "(-1.20,0.35)",
+        "(-1,0.20)",
+    ], stdout=PIPE)
+    (output, err) = process.communicate()
+
+    output = output.split(' ')
+    del output[-1]
+    output = np.array(map(int, output)).reshape(int(height), int(width))
+
+    imshow(output)
+    show()
